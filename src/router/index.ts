aa5dc1c185dd9d3f419587707724b706b7cb78c3
Leeyhong <39257265+Leeyhong@users.noreply.github.com>
2023-05-05 11:57:10 +0800

    Add files via upload

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,1 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+const routes: RouteRecordRaw[] = [
+  { 
+    path:"/", 
+    redirect: '/login'
+  },
+  { 
+    path:"/home", 
+    component:()=>import("@/views/index.vue") 
+  },
+  { 
+    path:"/login", 
+    component:()=>import("@/views/login.vue") 
+  },
+]
 
+const router = createRouter({
+  history: createWebHistory(), // 采用history模式，不带#
+  // history: createWebHashHistory(), // 采用hash模式，带#
+  routes,
+})
+
+export default router
