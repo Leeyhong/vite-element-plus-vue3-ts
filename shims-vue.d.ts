e94b5ff1783556a0bee58636c5861b28eeeed01d
Leeyhong <39257265+Leeyhong@users.noreply.github.com>
2023-05-05 11:54:36 +0800

    Add files via upload

---
+++ shims-vue.d.ts
@@ -0,0 +1,8 @@
+// 文件作用：typescript 只能理解 .ts 文件，无法理解 .vue文件
+declare module '*.vue' {
+    import { DefineComponent } from 'vue'
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+    const component: DefineComponent<{}, {}, any>
+    export default component
+  }
+  
